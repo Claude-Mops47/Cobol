3bc16fb3a98be279a66080f6f6155a36db2d7252
Claude-Mops47 <mopenobiaclaude@gmail.com>
2022-11-17 22:16:57 +0100

    mon premier programme Cobol

---
+++ HelloWorld.cbl
@@ -0,0 +1,8 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLOWORLD.
+       AUTHOR. Claude.
+
+       PROCEDURE DIVISION.
+           DISPLAY "Hello World MorningStar".
+
+           STOP RUN.
