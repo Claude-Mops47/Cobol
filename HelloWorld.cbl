536efa005904cda05a64eae227223888b4ca4bba
Claude-Mops47 <mopenobiaclaude@gmail.com>
2022-11-17 22:30:07 +0100

    test reussi

--- HelloWorld.cbl
+++ HelloWorld.cbl
@@ -2,7 +2,15 @@
        PROGRAM-ID. HELLOWORLD.
        AUTHOR. Claude.
 
+       DATA DIVISION. 
+       WORKING-STORAGE SECTION.
+       01 WS-NOM  PIC X(12).
+
        PROCEDURE DIVISION.
-           DISPLAY "Hello World MorningStar".
+           DISPLAY "Bienvenue dans mon petit programme".
+           DISPLAY 'Quel est ton nom ?'.
+
+           ACCEPT WS-NOM.
+           DISPLAY 'Et bien salut à toi ' WS-NOM.
 
            STOP RUN.
